3271732dc2022030ceafc2ef64eabebc748f0db9
Tomás Lara <tmslara.a@gmail.com>
2019-05-08 16:48:02 -0400

    Construccion de Baseline

--- seleccionCaracteristicas.m
+++ seleccionCaracteristicas.m
@@ -1,3 +1,10 @@
+function [] = TySConSeleccion()
+    %Modelo de Takagi Sugeno con la seleccion de caracteristicas y numero
+    %de reglas utilizando el clustering y analisis de sensibilidad.
+
+end
+
+
 function [XTrain,XVal,XTest] = seleccionCaracteristicas(XTrain,XVal,XTest,indicesEntradas)
     %Funcion que dado un vector de regresores a conservar modifica las
     %variables de entrada del modelo.
