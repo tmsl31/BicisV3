98c3c6c54e3bbfc27bf01bf38086b407bb52fa6e
Tomás Lara <tmslara.a@gmail.com>
2019-05-09 12:31:27 -0400

    Cambio configuracion de clusters

--- clusters_optimo.m
+++ clusters_optimo.m
@@ -12,7 +12,7 @@ errent=zeros(max_clusters-1,1);% se inicializa el vector de error de entreneamie
 % clusters
 pos=[];
 for i=2:max_clusters
-[model,result]=TakagiSugeno(yent,Xent,i,[1 4 2]);% en caso de no funcionar usar [2 2 1]
+[model,result]=TakagiSugeno(yent,Xent,i,[2 2 2]);% en caso de no funcionar usar [2 2 1]
 ps1=ps1+1;
 pos=[pos,ps1];
 y1=ysim(Xent,model.a,model.b,model.g);
