e9efeccb464ba5f9fb709ece3becc62ec9985476
Tomás Lara <tmslara.a@gmail.com>
2019-05-11 00:59:55 -0400

    Update baselineAutoregresivo2.m

--- baselineAutoregresivo2.m
+++ baselineAutoregresivo2.m
@@ -9,7 +9,8 @@ function [params,XTrain,XTest,YTrain,YTest,YPredict,muYTrain,stdYTrain] = baseli
     nRegresores = input('Numero de regresores Maximo inicial:');
     
     %Generacion de la estrutura del modelo. 
-    [out,in] = estructuraModelo2(nRegresores, tipoModelo);
+    [in,inV,out,outV] = estructuraModelo2(nRegresores, tipoModelo);
+
     
     %% Division de conjuntos.
     
@@ -22,21 +23,23 @@ function [params,XTrain,XTest,YTrain,YTest,YPredict,muYTrain,stdYTrain] = baseli
     porcentajeVal = 20;
     disp(strcat('Porcentaje Train:'," ",string(porcentajeTrain),'%;Porcentaje Val:'," ",string(porcentajeVal),'%'))
     %Division de conjuntos
-    [XTrain,XVal,~,YTrain,YVal,YTest,~,~,muYTrain,stdYTrain] = divConjuntos(in,out,porcentajeTrain,porcentajeVal,norm);
-    
+    %Modelo ERROR.
+    [XTrain,XVal,XTest,YTrain,YVal,YTest,muXTrain,stdXTrain,muYTrain,stdYTrain] = divConjuntos(in,out,porcentajeTrain,porcentajeVal,norm);
+    %Modelo VELOCIDAD.
+    [XTrainV,XValV,XTestV,YTrainV,YValV,YTestV,muXTrainV,stdXTrainV,muYTrainV,stdYTrainV] = divConjuntos(inV,outV,porcentajeTrain,porcentajeVal,norm);
     %%Estadisticas.
     estadisticosError(YTrain,YVal,YTest);
     
     %% ANALISIS DE SENSIBILIDAD
-    sensibilidadRegresores(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain);
+    analisisSensibilidad(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain,tipoModelo)
     nRegresoresOptimo = input('Numero Optimo de Regresores: ');
     
     %% Construccion del modelo lineal -> Ojo: sin termino libre.
     
     %Estructura
-    [in,~,out,~] = estructuraModelo2(nRegresoresOptimo, tipoModelo);
+    [in,inV,out,outV] = estructuraModelo2(nRegresoresOptimo, tipoModelo);
     %Division de los conjuntos
-    [XTrain,~,XTest,YTrain,~,YTest,~,~,muYTrain,stdYTrain] = divConjuntos(in,out,porcentajeTrain,porcentajeVal,norm);
+    [XTrain,XVal,XTest,YTrain,YVal,YTest,muXTrain,stdXTrain,muYTrain,stdYTrain] = divConjuntos(in,out,porcentajeTrain,porcentajeVal,norm);
     % Obtencion de los parámetros utilizando LMS. Se utiliza el conjunto de
     % entrenamiento.
     params = (transpose(XTrain)*XTrain)^(-1)*transpose(XTrain)*YTrain;
@@ -105,6 +108,30 @@ function [] = estadisticosError(YTrain,YVal,YTest)
     disp(strcat('Maximo:',string(maxY)))
 end
 
+
+% ANALISIS DE SENSIBILIDAD
+
+%Funcion que diferencia entre modelos puramente autoregresivo y con
+%velocidad instantanea.
+
+function [] = analisisSensibilidad(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain,tipoModelo)
+    %Funcion que realiza el analisis de sensibilidad dependiendo del tipo
+    %de modelo.
+    
+    %Casos.
+    if(tipoModelo == 0)
+        %Caso de modelo puramente autoregresivo.
+        sensibilidadRegresores(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain);
+    elseif(tipoModelo == 1)
+        %Caso de modelo con error y velocidad instantanea.
+        sensibilidadRegresoresVelocidad(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain)
+    else
+        disp('OPCION INCORRECTA. ANALISIS DE SENSIBILIDAD')
+    end
+
+end
+
+%Sensibilidad Regresores.
 function [] = sensibilidadRegresores(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain)
     %Buscar el numero optimo de autoregresores. Se hace esto eliminando de
     %una fila en las entradas del modelo y evaluar. Puede no alcanzarse el
@@ -141,3 +168,48 @@ function [] = sensibilidadRegresores(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain)
     plot(vecRegresores,vectorRMSE)
     hold off
 end
+
+function [] = sensibilidadRegresoresVelocidad(XTrain,XVal,YTrain,YVal,muYTrain,stdYTrain)
+    %Analisis de sensibilidad para modelo con regresores y velocidad. Se
+    %eliminan regresores de error y velocidad simultaneamente. No
+    %necesariamente optimo.
+    
+    %Dimensiones originales.
+    [~,nRegresores] = size(XTrain);
+    %Vector que almacene los valores de RMSE
+    vectorRMSE = zeros(nRegresores,1);
+    
+    %Ciclo de eliminacion y evaluacion.
+    count = 1;
+    %Vector de regresores.
+    vecRegresores = nRegresosres:-1:1;
+    %Loop
+    for i = vecRegresores
+        X1 = XTrain;
+        %Eliminacion de entradas entrenamiento.
+        X1(:,[nRegresores:-1:i,(nRegresores + nRegresores:-1:i)]) = [];
+        Y1 = YTrain;
+        %Eliminacion de entradas validacion
+        X2 = XVal;
+        X2(:,[nRegresores:-1:i,(nRegresores + nRegresores:-1:i)]) = [];
+        Y2 = YVal;
+        %Obtencion de parametros
+        params = (transpose(X1)*X1)^(-1)*transpose(X1)*Y1;
+        %Evaluacion del modelo.
+        [~,~,RMSEBaseline] = evaluacionBaseline(params,X2,Y2,muYTrain,stdYTrain);
+        vectorRMSE(count) = RMSEBaseline;
+        count = count + 1;        
+    end
+    
+    %plot
+    figure()
+    hold on
+    title('RMSE vs Numero de regresores.')
+    ylabel('RMSE [m/s^2]')
+    xlabel('Numero de regresores')
+    plot(vecRegresores,vectorRMSE)
+    hold off
+    
+    
+end
+
