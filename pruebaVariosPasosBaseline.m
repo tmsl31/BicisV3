f28e94a6609d93b7555453ae62433a1f1fd4571f
Tomás Lara <tmslara.a@gmail.com>
2019-05-13 10:24:13 -0400

    Adaptación a dos tipos de modelos.

--- pruebaVariosPasosBaseline.m
+++ pruebaVariosPasosBaseline.m
@@ -1,4 +1,4 @@
-function [vecRMSE] = pruebaVariosPasosBaseline(parametros,nPasos,XTest,YTest,muYTrain,stdYTrain) 
+function [vecRMSE] = pruebaVariosPasosBaseline(parametros,nPasos,XTest,YTest,muYTrain,stdYTrain,tipoModelo) 
     %Realiza evaluacion de RMSE para diferentes numeros de pasos.
     
     %Numero Predicciones
@@ -8,9 +8,27 @@ function [vecRMSE] = pruebaVariosPasosBaseline(parametros,nPasos,XTest,YTest,muY
     %Loop
     count = 1;
     for pasos = nPasos
-        [YPredict,Y] = predictPasosBaseline(parametros,pasos,XTest,YTest,muYTrain,stdYTrain);
+        [YPredict,Y] = predictPasosBaseline(parametros,pasos,XTest,YTest,muYTrain,stdYTrain,tipoModelo);
         vecRMSE(count) = RMSE(YPredict,Y);
         count = count + 1;
     end
+    
+    if (tipoModelo == 0)
+        %Modelo Autoregresivo.
+        modelo = 'Autoregresivo';       
+    elseif(tipoModelo == 1)
+        %Auto regresivo + velocidad
+        modelo = 'Autoregresivo+Velocidad';
+    else
+        disp('NOOOOOOOOOOOOOOOO')
+    end
+    %Plot
+    figure()
+    hold on
+    plot(vecRMSE,'-o')
+    xlabel('Número de pasos')
+    ylabel('RMSE')
+    title(strcat('RMSE versus horizonte de predicción. Modelo: ',modelo))
+    hold off
 end
 
